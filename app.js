904e52801a18a403eb995e194201106a3cdd5c43
tighnaridotme <35395586+tighnaridotme@users.noreply.github.com>
2024-12-21 03:11:44 +0100

    Make use of token's ttl

--- app.js
+++ app.js
@@ -34,54 +34,65 @@ async function fetchSpotifyToken() {
     }
 }
 
-app.get('/api/recent-track', async(req, res) => {
-    const url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&format=json&extended=true&api_key=${process.env.LASTFM_API_KEY}&limit=1&user=${process.env.LASTFM_USERNAME}`;
+async function main() {
+    var token = null;
+    var timestamp = Date.now();
 
-    try {
-        const lastfmResponse = await fetch(url);
-        const lastfmData = await lastfmResponse.json();
+    app.get('/api/recent-track', async(req, res) => {
+        const url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&format=json&extended=true&api_key=${process.env.LASTFM_API_KEY}&limit=1&user=${process.env.LASTFM_USERNAME}`;
 
-        const lastTrack = lastfmData.recenttracks.track[0];
+        try {
+            const lastfmResponse = await fetch(url);
+            const lastfmData = await lastfmResponse.json();
 
-        const token = await fetchSpotifyToken();
-        var query = encodeURIComponent(`track:${lastTrack.name} artist:${lastTrack.artist.name}`);
-        query = encodeURIComponent(query);
-        const spotifyResponse = await fetch(`https://api.spotify.com/v1/search?q=${query}&type=track&market=US&limit=10`, {
-            headers: {
-                Authorization: 'Bearer ' + token
+            const lastTrack = lastfmData.recenttracks.track[0];
+
+            if (!token || Date.now() - timestamp > 3600000) {
+                timestamp = Date.now();
+                token = await fetchSpotifyToken();
             }
-        });
 
-        const spotifyData = await spotifyResponse.json();
-        var spotifyImage = null;
-        var spotifyExtUrl = null;
-        if (Object.keys(spotifyData.tracks.items).length) {
-            for (let i = 0; i < Object.keys(spotifyData.tracks.items).length; i++) {
-                if ((spotifyData.tracks.items[i].name === lastTrack.name) && (spotifyData.tracks.items[i].artists.find(x => x.name === lastTrack.artist.name))) {
-                    spotifyImage = spotifyData.tracks.items[i].album.images?.find(x => x.height === 300)?.url;
-                    spotifyExtUrl = spotifyData.tracks.items[i].external_urls.spotify;
-                    break;
+            var query = encodeURIComponent(`track:${lastTrack.name} artist:${lastTrack.artist.name}`);
+            query = encodeURIComponent(query);
+            const spotifyResponse = await fetch(`https://api.spotify.com/v1/search?q=${query}&type=track&market=US&limit=10`, {
+                headers: {
+                    Authorization: 'Bearer ' + token
+                }
+            });
+
+            const spotifyData = await spotifyResponse.json();
+            var spotifyImage = null;
+            var spotifyExtUrl = null;
+            if (Object.keys(spotifyData.tracks.items).length) {
+                for (let i = 0; i < Object.keys(spotifyData.tracks.items).length; i++) {
+                    if ((spotifyData.tracks.items[i].name === lastTrack.name) && (spotifyData.tracks.items[i].artists.find(x => x.name === lastTrack.artist.name))) {
+                        spotifyImage = spotifyData.tracks.items[i].album.images?.find(x => x.height === 300)?.url;
+                        spotifyExtUrl = spotifyData.tracks.items[i].external_urls.spotify;
+                        break;
+                    }
                 }
             }
+
+            res.json({
+                track: lastTrack.name,
+                trackLink: lastTrack.url,
+                artist: lastTrack.artist.name,
+                artistLink: lastTrack.artist.url,
+                albumCover: spotifyImage ? spotifyImage : lastTrack.image?.find(x => x.size === "large")?.["#text"],
+                date: lastTrack.date ? lastTrack.date.uts : null,
+                spotifyLink: spotifyExtUrl
+            });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({
+                error: "Failed to fetch recent track."
+            });
         }
+    });
 
-        res.json({
-            track: lastTrack.name,
-            trackLink: lastTrack.url,
-            artist: lastTrack.artist.name,
-            artistLink: lastTrack.artist.url,
-            albumCover: spotifyImage ? spotifyImage : lastTrack.image?.find(x => x.size === "large")?.["#text"],
-            date: lastTrack.date ? lastTrack.date.uts : null,
-            spotifyLink: spotifyExtUrl
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            error: "Failed to fetch recent track."
-        });
-    }
-});
+    app.listen(process.env.PORT, () => {
+        console.log(`Backend running on http://localhost:${process.env.PORT}`);
+    });
+}
 
-app.listen(process.env.PORT, () => {
-    console.log(`Backend running on http://localhost:${process.env.PORT}`);
-});
+main();
